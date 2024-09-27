0578924c0b51067d19b8ef6a06219b8c596a9652
nahidrahat <nahidrahat007@gmail.com>
2024-09-27 23:26:39 +0600

    added

--- bin/main.dart
+++ bin/main.dart
@@ -2,11 +2,11 @@ import 'car_class.dart';
 
 void main(){
 
-  Car car = Car(brand: 'Toyota', model: 'Corolla' , year: 2020);
+  Car car = Car(brand: 'Toyota', model: 'Corolla' , year: 2015);
 
   print('Brand: ${car.brand}');
   print('Model: ${car.model}');
-  print('Years: ${car.year}');
+  print('Year: ${car.year}');
   print('Car Age ${car.carAge()} Years');
 
 
